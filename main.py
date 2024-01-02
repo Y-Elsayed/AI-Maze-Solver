201f3c51bc43747beadaae04259c42ed0edd48ea
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-01-02 21:38:17 +0200

    Update main.py
    The testing done to generate the demo video

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from pyamaze import maze, agent, COLOR
+from mazeworld import maze, agent, COLOR
 from Astar import Astar
 from BFS import BFS
 from DFS import DFS
 
-m = maze(15,15)
-m.CreateMaze(3, 5, loopPercent=100, loadMaze='maze2.csv')
+m = maze(30, 30)
+m.CreateMaze(loopPercent=80, loadMaze='30x30_maze.csv')
 
-searchPath, algorithmPath, forwardPath = DFS(m, goal=(3,5))
+searchPath, algorithmPath, forwardPath = Astar(m)
 
-searchAgent = agent(m, footprints=True, filled=True, color=COLOR.blue)
-algorithmAgent = agent(m, 3, 5, footprints=True, filled=True, color=COLOR.yellow, goal=(m.rows, m.cols))
-forwardAgent = agent(m, footprints=True, color=COLOR.dark)
+# searchAgent = agent(m, footprints=True, filled=True, color=COLOR.blue)
+# algorithmAgent = agent(m, 1, 1, footprints=True, filled=True, color=COLOR.green, goal=(m.rows, m.cols))
+# forwardAgent = agent(m, 30, 1, footprints=True, color=COLOR.dark)
 
-m.tracePath({searchAgent:searchPath}, delay=50)
-m.tracePath({algorithmAgent:algorithmPath}, delay=100)
-m.tracePath({forwardAgent:forwardPath}, delay=100)
+# m.tracePath({searchAgent:searchPath}, delay=300)
+# m.tracePath({algorithmAgent:algorithmPath}, delay=250)
+# m.tracePath({forwardAgent:forwardPath}, delay=225)
+
+# algorithmAgent = agent(m, 1, 1, footprints=True, filled=True, color=COLOR.green)
+# searchAgent = agent(m, footprints=True, filled=True, color=COLOR.blue)
+# forwardAgent = agent(m, 30, 1, footprints=True, color=COLOR.yellow)
 
 m.run()
